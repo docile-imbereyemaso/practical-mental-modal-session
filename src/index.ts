bbd88acb2c1da21737913dd90d751e299dd4c57e
Docile123 <imbereyemasoo@gmail.com>
2026-07-02 14:28:10 +0200

    fix: remove unneccessary console

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,4 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use("/items", itemRoutes);
 app.use(errorHandler);
-app.listen(3000, () => console.log(`Server is running on port: 3000 `));
+app.listen(3000);
